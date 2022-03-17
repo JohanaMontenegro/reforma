1f9f4cda41ad56d17bc898fcdb3a2a2ece0086d6
JohanaMontenegro <92064090+JohanaMontenegro@users.noreply.github.com>
2022-03-16 22:29:05 -0300

    Initial commit

---
+++ src/index.ts
@@ -0,0 +1,5 @@
+let cadenaText: string = " En las vacaciones no Hice pileta ni fui a la PLaya";
+let largoDePalabra: number = 50;
+
+for (let i: number; i = largoDePalabra.lengt); i++) 
+cadenaText.indexOf(largoDePalabra[i]);
